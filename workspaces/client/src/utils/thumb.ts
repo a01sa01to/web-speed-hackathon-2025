845b8716dbe238e37b3f0a1aa610b154dced3107
Asa <26807394+a01sa01to@users.noreply.github.com>
2025-03-22 20:14:59 +0900

    fix error

--- workspaces/client/src/utils/thumb.ts
+++ workspaces/client/src/utils/thumb.ts
@@ -1,5 +1,5 @@
 export const thumbUrl = (url: string, size: "sm" | "md" | "lg"): string => {
-  const thumburl = new URL(url)
+  const thumburl = new URL(url, "http://localhost:8000")
   thumburl.search = ""
   thumburl.pathname = thumburl.pathname.replace(".jpeg", `-${size}.avif`)
   return `https://wsh2025-a01sa01to.pages.dev${thumburl.pathname}`
