fe64845a89a860cb5ede16188668096899e9949d
Asa <26807394+a01sa01to@users.noreply.github.com>
2025-03-22 15:26:06 +0900

    SSR やめる

--- workspaces/server/src/ssr.tsx
+++ workspaces/server/src/ssr.tsx
@@ -2,15 +2,15 @@ import path from 'node:path';
 import { fileURLToPath } from 'node:url';
 
 import fastifyStatic from '@fastify/static';
-import { StoreProvider } from '@wsh-2025/client/src/app/StoreContext';
-import { createRoutes } from '@wsh-2025/client/src/app/createRoutes';
-import { createStore } from '@wsh-2025/client/src/app/createStore';
+// import { StoreProvider } from '@wsh-2025/client/src/app/StoreContext';
+// import { createRoutes } from '@wsh-2025/client/src/app/createRoutes';
+// import { createStore } from '@wsh-2025/client/src/app/createStore';
 import type { FastifyInstance } from 'fastify';
-import { createStandardRequest } from 'fastify-standard-request-reply';
-import htmlescape from 'htmlescape';
-import { StrictMode } from 'react';
-import { renderToString } from 'react-dom/server';
-import { createStaticHandler, createStaticRouter, StaticRouterProvider } from 'react-router';
+// import { createStandardRequest } from 'fastify-standard-request-reply';
+// import htmlescape from 'htmlescape';
+// import { StrictMode } from 'react';
+// import { renderToString } from 'react-dom/server';
+// import { createStaticHandler, createStaticRouter, StaticRouterProvider } from 'react-router';
 
 export function registerSsr(app: FastifyInstance): void {
   app.register(fastifyStatic, {
@@ -27,24 +27,24 @@ export function registerSsr(app: FastifyInstance): void {
 
   app.get('/*', async (req, reply) => {
     // @ts-expect-error ................
-    const request = createStandardRequest(req, reply);
+    // const request = createStandardRequest(req, reply);
 
-    const store = createStore({});
-    const handler = createStaticHandler(createRoutes(store));
-    const context = await handler.query(request);
+    // const store = createStore({});
+    // const handler = createStaticHandler(createRoutes(store));
+    // const context = await handler.query(request);
 
-    if (context instanceof Response) {
-      return reply.send(context);
-    }
+    // if (context instanceof Response) {
+    //   return reply.send(context);
+    // }
 
-    const router = createStaticRouter(handler.dataRoutes, context);
-    renderToString(
-      <StrictMode>
-        <StoreProvider createStore={() => store}>
-          <StaticRouterProvider context={context} hydrate={false} router={router} />
-        </StoreProvider>
-      </StrictMode>,
-    );
+    // const router = createStaticRouter(handler.dataRoutes, context);
+    // renderToString(
+    //   <StrictMode>
+    //     <StoreProvider createStore={() => store}>
+    //       <StaticRouterProvider context={context} hydrate={false} router={router} />
+    //     </StoreProvider>
+    //   </StrictMode>,
+    // );
 
     reply.type('text/html').send(/* html */ `
       <!DOCTYPE html>
@@ -56,12 +56,12 @@ export function registerSsr(app: FastifyInstance): void {
         </head>
         <body></body>
       </html>
-      <script>
+      ${/*<script>
         window.__staticRouterHydrationData = ${htmlescape({
       actionData: context.actionData,
       loaderData: context.loaderData,
     })};
-      </script>
+      </script>*/""}
     `);
   });
 }
