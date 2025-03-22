6273879a94e0d98f14a3aefe3ecc2fa7fd54832e
Asa <26807394+a01sa01to@users.noreply.github.com>
2025-03-22 20:04:55 +0900

    サムネイルも Cloudflare 任せ

--- workspaces/client/src/utils/thumb.ts
+++ workspaces/client/src/utils/thumb.ts
@@ -1,4 +1,9 @@
-export const thumbUrl = (url: string, size: "sm" | "md" | "lg"): string => url.split("?")[0]?.replace(".jpeg", `-${size}.avif`) ?? ""
+export const thumbUrl = (url: string, size: "sm" | "md" | "lg"): string => {
+  const thumburl = new URL(url)
+  thumburl.search = ""
+  thumburl.pathname = thumburl.pathname.replace(".jpeg", `-${size}.avif`)
+  return `https://wsh2025-a01sa01to.pages.dev${thumburl.pathname}`
+}
 
 // sm: w200
 // md: w500
