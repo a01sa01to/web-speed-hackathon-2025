52673a6413b12f94cded44c5c9c1ab689a32d135
Asa <26807394+a01sa01to@users.noreply.github.com>
2025-03-22 20:18:42 +0900

    fix thumb path

--- workspaces/client/src/utils/thumb.ts
+++ workspaces/client/src/utils/thumb.ts
@@ -1,7 +1,7 @@
 export const thumbUrl = (url: string, size: "sm" | "md" | "lg"): string => {
   const thumburl = new URL(url, "http://localhost:8000")
   thumburl.search = ""
-  thumburl.pathname = thumburl.pathname.replace(".jpeg", `-${size}.avif`)
+  thumburl.pathname = thumburl.pathname.replace(".jpeg", `-${size}.avif`).replace("/public", "")
   return `https://wsh2025-a01sa01to.pages.dev${thumburl.pathname}`
 }
 
