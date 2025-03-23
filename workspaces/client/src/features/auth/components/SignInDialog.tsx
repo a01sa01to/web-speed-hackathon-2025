7e1c041333a01076107a79307fd26ef5af57367c
Asa <26807394+a01sa01to@users.noreply.github.com>
2025-03-23 15:08:19 +0900

    root と features/auth の CSS 化

--- workspaces/client/src/features/auth/components/SignInDialog.tsx
+++ workspaces/client/src/features/auth/components/SignInDialog.tsx
@@ -44,159 +44,123 @@ export const SignInDialog = ({ isOpen, onClose, onOpenSignUp }: Props) => {
   };
 
   return (
-    <Dialog isOpen={isOpen} onClose={onClose}>
-      <div style={{ height: '100%', width: '100%' }}>
-        <div style={{ display: 'flex', flexDirection: 'row', justifyContent: 'center', marginBottom: '16px', width: '100%' }}>
-          {/* biome-ignore lint/a11y/useAltText: <explanation> */}
-          <img height={36} src="https://wsh2025-a01sa01to.pages.dev/arema.svg" style={{ objectFit: 'contain' }} width={98} />
-        </div>
+    <>
+      <link href="/public/styles/feat/auth/signin.css" rel="stylesheet" />
 
-        <h2 style={{ fontSize: '24px', fontWeight: 'bold', marginBottom: '24px', textAlign: 'center' }}>ログイン</h2>
+      <Dialog isOpen={isOpen} onClose={onClose}>
+        <div className="a-adiv">
+          <div className='a-bdiv'>
+            {/* biome-ignore lint/a11y/useAltText: <explanation> */}
+            <img className="a-img" height={36} src="https://wsh2025-a01sa01to.pages.dev/arema.svg" width={98} />
+          </div>
 
-        <Form
-          validate={(values) => {
-            const schema = z.object({
-              email: z
-                .string({ required_error: 'メールアドレスを入力してください' })
-                .and(z.custom(isValidEmail, { message: 'メールアドレスが正しくありません' })),
-              password: z
-                .string({ required_error: 'パスワードを入力してください' })
-                .and(z.custom(isValidPassword, { message: 'パスワードが正しくありません' })),
-            });
-            const result = schema.safeParse(values);
-            return result.success ? undefined : result.error.formErrors.fieldErrors;
-          }}
-          onSubmit={onSubmit}
-        >
-          {({ handleSubmit, hasValidationErrors, submitError, submitting }) => (
-            <form style={{ marginBottom: '16px' }} onSubmit={(ev) => void handleSubmit(ev)}>
-              <style>{`
-                input::placeholder {
-                  color: #999999;
-                }
-              `}</style>
-              <Field name="email">
-                {({ input, meta }) => {
-                  return (
-                    <div style={{ marginBottom: '24px' }}>
-                      <div style={{ alignItems: 'center', display: 'flex', flexDirection: 'row', fontSize: '14px', fontWeight: 'bold', justifyContent: 'space-between', marginBottom: '8px' }}>
-                        <label htmlFor={emailId} style={{ flexGrow: 0, flexShrink: 0 }}>
-                          メールアドレス
-                        </label>
-                        {meta.modified && Array.isArray(meta.error) ? (
-                          <span style={{ color: '#F0163A', flexGrow: 0, flexShrink: 0 }}>{meta.error[0]}</span>
-                        ) : null}
-                      </div>
-                      <input
-                        {...input}
-                        required
-                        id={emailId}
-                        placeholder="メールアドレスを入力"
-                        style={{
-                          backgroundColor: '#FFFFFF',
-                          border: '2px solid #FFFFFF1F',
-                          borderRadius: '4px',
-                          color: '#212121',
-                          fontSize: '14px',
-                          padding: '12px',
-                          width: '100%',
-                        }}
-                        type="email"
-                      />
-                    </div>
-                  );
-                }}
-              </Field>
+          <h2 className='a-h2'>ログイン</h2>
 
-              <Field name="password">
-                {({ input, meta }) => {
-                  return (
-                    <div style={{ marginBottom: '24px' }}>
-                      <div style={{ alignItems: 'center', display: 'flex', flexDirection: 'row', fontSize: '14px', fontWeight: 'bold', justifyContent: 'space-between', marginBottom: '8px' }}>
-                        <label htmlFor={passwordId} style={{ flexGrow: 0, flexShrink: 0 }}>
-                          パスワード
-                        </label>
-                        {meta.modified && Array.isArray(meta.error) ? (
-                          <span style={{ color: '#F0163A', flexGrow: 0, flexShrink: 0 }}>{meta.error[0]}</span>
-                        ) : null}
-                      </div>
-                      <input
-                        {...input}
-                        required
-                        id={passwordId}
-                        placeholder="パスワードを入力"
-                        style={{
-                          backgroundColor: '#FFFFFF',
-                          border: '2px solid #FFFFFF1F',
-                          borderRadius: '4px',
-                          color: '#212121',
-                          fontSize: '14px',
-                          padding: '12px',
-                          width: '100%',
-                        }}
-                        type="password"
-                      />
-                    </div>
-                  );
-                }}
-              </Field>
-
-              {submitError ? (
-                <div style={{ alignItems: 'center', backgroundColor: '#ffeeee', border: '2px solid #F0163A', borderRadius: '4px', color: '#F0163A', display: 'flex', flexDirection: 'row', fontSize: '14px', fontWeight: 'bold', justifyContent: 'flex-start', marginBottom: '8px', padding: '8px', width: "100%" }}>
-                  {/* biome-ignore lint/a11y/noSvgWithoutTitle: <explanation> */}
-                  <svg
-                    style={{ height: '20px', margin: '4px', width: '20px' }}
-                    viewBox="0 0 24 24"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path d="M12 17q.425 0 .713-.288T13 16t-.288-.712T12 15t-.712.288T11 16t.288.713T12 17m-1-4h2V7h-2zm1 9q-2.075 0-3.9-.788t-3.175-2.137T2.788 15.9T2 12t.788-3.9t2.137-3.175T8.1 2.788T12 2t3.9.788t3.175 2.137T21.213 8.1T22 12t-.788 3.9t-2.137 3.175t-3.175 2.138T12 22m0-2q3.35 0 5.675-2.325T20 12t-2.325-5.675T12 4T6.325 6.325T4 12t2.325 5.675T12 20m0-8" fill="currentColor" />
-                  </svg>
-                  <span>{submitError}</span>
-                </div>
-              ) : null}
-
-              <div style={{ display: 'flex', flexDirection: 'row', justifyContent: 'center' }}>
-                <button
-                  disabled={submitting || hasValidationErrors}
-                  style={{
-                    alignItems: 'center',
-                    backgroundColor: '#1c43d1',
-                    borderRadius: '4px',
-                    color: '#ffffff',
-                    display: 'flex',
-                    flexDirection: "row",
-                    fontSize: '14px',
-                    fontWeight: 'bold',
-                    justifyContent: 'center',
-                    opacity: submitting || hasValidationErrors ? 0.5 : 1,
-                    padding: '12px',
-                    width: '160px',
-                  }}
-                  type="submit"
-                >
-                  ログイン
-                </button>
-              </div>
-            </form>
-          )}
-        </Form>
-
-        <div style={{ display: 'flex', flexDirection: 'row', justifyContent: 'center' }}>
-          <button
-            style={{
-              backgroundColor: 'transparent',
-              color: '#999999',
-              display: 'block',
-              fontSize: '14px',
-              textDecoration: 'underline',
+          <Form
+            validate={(values) => {
+              const schema = z.object({
+                email: z
+                  .string({ required_error: 'メールアドレスを入力してください' })
+                  .and(z.custom(isValidEmail, { message: 'メールアドレスが正しくありません' })),
+                password: z
+                  .string({ required_error: 'パスワードを入力してください' })
+                  .and(z.custom(isValidPassword, { message: 'パスワードが正しくありません' })),
+              });
+              const result = schema.safeParse(values);
+              return result.success ? undefined : result.error.formErrors.fieldErrors;
             }}
-            type="button"
-            onClick={onOpenSignUp}
+            onSubmit={onSubmit}
           >
-            アカウントを新規登録する
-          </button>
+            {({ handleSubmit, hasValidationErrors, submitError, submitting }) => (
+              <form className="a-form" onSubmit={(ev) => void handleSubmit(ev)}>
+                <Field name="email">
+                  {({ input, meta }) => {
+                    return (
+                      <div className="a-form-div">
+                        <div className="a-form-div2">
+                          <label className="a-form-divlabel" htmlFor={emailId} >
+                            メールアドレス
+                          </label>
+                          {meta.modified && Array.isArray(meta.error) ? (
+                            <span className="a-form-divspan">{meta.error[0]}</span>
+                          ) : null}
+                        </div>
+                        <input
+                          {...input}
+                          required
+                          className="a-input"
+                          id={emailId}
+                          placeholder="メールアドレスを入力"
+                          type="email"
+                        />
+                      </div>
+                    );
+                  }}
+                </Field>
+
+                <Field name="password">
+                  {({ input, meta }) => {
+                    return (
+                      <div className="a-form-div">
+                        <div className="a-form-div2">
+                          <label className="a-form-divlabel" htmlFor={passwordId} >
+                            パスワード
+                          </label>
+                          {meta.modified && Array.isArray(meta.error) ? (
+                            <span className="a-form-divspan">{meta.error[0]}</span>
+                          ) : null}
+                        </div>
+                        <input
+                          {...input}
+                          required
+                          className="a-input"
+                          id={passwordId}
+                          placeholder="パスワードを入力"
+                          type="password"
+                        />
+                      </div>
+                    );
+                  }}
+                </Field>
+
+                {submitError ? (
+                  <div className="a-suberrdiv">
+                    {/* biome-ignore lint/a11y/noSvgWithoutTitle: <explanation> */}
+                    <svg
+                      className="a-suberrSvg"
+                      viewBox="0 0 24 24"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path d="M12 17q.425 0 .713-.288T13 16t-.288-.712T12 15t-.712.288T11 16t.288.713T12 17m-1-4h2V7h-2zm1 9q-2.075 0-3.9-.788t-3.175-2.137T2.788 15.9T2 12t.788-3.9t2.137-3.175T8.1 2.788T12 2t3.9.788t3.175 2.137T21.213 8.1T22 12t-.788 3.9t-2.137 3.175t-3.175 2.138T12 22m0-2q3.35 0 5.675-2.325T20 12t-2.325-5.675T12 4T6.325 6.325T4 12t2.325 5.675T12 20m0-8" fill="currentColor" />
+                    </svg>
+                    <span>{submitError}</span>
+                  </div>
+                ) : null}
+
+                <div className="a-subdiv">
+                  <button
+                    className="a-subbtn"
+                    disabled={submitting || hasValidationErrors}
+                    type="submit"
+                  >
+                    ログイン
+                  </button>
+                </div>
+              </form>
+            )}
+          </Form>
+
+          <div className="a-regdiv">
+            <button
+              className="a-regbtn"
+              type="button"
+              onClick={onOpenSignUp}
+            >
+              アカウントを新規登録する
+            </button>
+          </div>
         </div>
-      </div>
-    </Dialog>
+      </Dialog>
+    </>
   );
 };
--- workspaces/client/src/features/auth/components/SignOutDialog.tsx
+++ workspaces/client/src/features/auth/components/SignOutDialog.tsx
@@ -25,50 +25,51 @@ export const SignOutDialog = ({ isOpen, onClose }: Props) => {
   };
 
   return (
-    <Dialog isOpen={isOpen} onClose={onClose}>
-      <div style={{ height: "100%", width: "100%" }}>
-        <div style={{ display: "flex", flexDirection: "row", justifyContent: "center", marginBottom: "16px", width: "100%" }}>
-          {/* biome-ignore lint/a11y/useAltText: <explanation> */}
-          <img height={36} src="https://wsh2025-a01sa01to.pages.dev/arema.svg" style={{ objectFit: "contain" }} width={98} />
-        </div>
+    <>
+      <link href="/public/styles/feat/auth/signout.css" rel="stylesheet" />
 
-        <h2 style={{ fontSize: "24px", fontWeight: "bold", marginBottom: "24px", textAlign: "center" }}>ログアウト</h2>
+      <Dialog isOpen={isOpen} onClose={onClose}>
+        <div className="b-div1">
+          <div className="b-div2">
+            {/* biome-ignore lint/a11y/useAltText: <explanation> */}
+            <img className="b-img" height={36} src="https://wsh2025-a01sa01to.pages.dev/arema.svg" width={98} />
+          </div>
 
-        <Form onSubmit={onSubmit}>
-          {({ handleSubmit, submitError }) => (
-            <form style={{ marginBottom: "16px" }} onSubmit={(ev) => void handleSubmit(ev)}>
-              <div style={{ alignItems: "center", backgroundColor: "#fffcee", border: "2px solid #DDAA00", borderRadius: "4px", color: "#DDAA00", display: "flex", flexDirection: "row", fontSize: "14px", fontWeight: "bold", justifyContent: "start", marginBottom: "24px", padding: "8px", width: "100%" }}>
-                {/* biome-ignore lint/a11y/noSvgWithoutTitle: <explanation> */}
-                <svg height="1em" style={{ height: "20px", margin: "4px", width: "20px" }} viewBox="0 0 24 24" width="1em"
-                  xmlns="http://www.w3.org/2000/svg">
-                  <path d="M2.725 21q-.275 0-.5-.137t-.35-.363t-.137-.488t.137-.512l9.25-16q.15-.25.388-.375T12 3t.488.125t.387.375l9.25 16q.15.25.138.513t-.138.487t-.35.363t-.5.137zm1.725-2h15.1L12 6zM12 18q.425 0 .713-.288T13 17t-.288-.712T12 16t-.712.288T11 17t.288.713T12 18m0-3q.425 0 .713-.288T13 14v-3q0-.425-.288-.712T12 10t-.712.288T11 11v3q0 .425.288.713T12 15m0-2.5" fill="currentColor" />
-                </svg>
-                <span>プレミアムエピソードが視聴できなくなります。</span>
-              </div>
+          <h2 className="b-h2">ログアウト</h2>
 
-              {submitError ? (
-                <div style={{ alignItems: "center", backgroundColor: "#ffeeee", border: "2px solid #F0163A", borderRadius: "4px", color: "#F0163A", display: "flex", flexDirection: "row", fontSize: "14px", fontWeight: "bold", justifyContent: "start", marginBottom: "8px", padding: "8px", width: "100%" }}>
+          <Form onSubmit={onSubmit}>
+            {({ handleSubmit, submitError }) => (
+              <form className="b-form" onSubmit={(ev) => void handleSubmit(ev)}>
+                <div className="b-form-div">
                   {/* biome-ignore lint/a11y/noSvgWithoutTitle: <explanation> */}
-                  <svg height="1em" style={{ height: "20px", margin: "4px", width: "20px" }} viewBox="0 0 24 24" width="1em"
+                  <svg className="b-form-svg" viewBox="0 0 24 24"
                     xmlns="http://www.w3.org/2000/svg">
-                    <path d="M12 17q.425 0 .713-.288T13 16t-.288-.712T12 15t-.712.288T11 16t.288.713T12 17m-1-4h2V7h-2zm1 9q-2.075 0-3.9-.788t-3.175-2.137T2.788 15.9T2 12t.788-3.9t2.137-3.175T8.1 2.788T12 2t3.9.788t3.175 2.137T21.213 8.1T22 12t-.788 3.9t-2.137 3.175t-3.175 2.138T12 22m0-2q3.35 0 5.675-2.325T20 12t-2.325-5.675T12 4T6.325 6.325T4 12t2.325 5.675T12 20m0-8" fill="currentColor" />
+                    <path d="M2.725 21q-.275 0-.5-.137t-.35-.363t-.137-.488t.137-.512l9.25-16q.15-.25.388-.375T12 3t.488.125t.387.375l9.25 16q.15.25.138.513t-.138.487t-.35.363t-.5.137zm1.725-2h15.1L12 6zM12 18q.425 0 .713-.288T13 17t-.288-.712T12 16t-.712.288T11 17t.288.713T12 18m0-3q.425 0 .713-.288T13 14v-3q0-.425-.288-.712T12 10t-.712.288T11 11v3q0 .425.288.713T12 15m0-2.5" fill="currentColor" />
                   </svg>
-                  <span>{submitError}</span>
+                  <span>プレミアムエピソードが視聴できなくなります。</span>
                 </div>
-              ) : null}
 
-              <div style={{ display: "flex", flexDirection: "row", justifyContent: "center" }}>
-                <button
-                  style={{ alignItems: "center", backgroundColor: "#1c43d1", borderRadius: "4px", color: "#ffffff", display: "flex", flexDirection: "row", fontSize: "14px", fontWeight: "bold", justifyContent: "center", padding: "12px", width: "160px" }}
-                  type="submit"
-                >
-                  ログアウト
-                </button>
-              </div>
-            </form>
-          )}
-        </Form>
-      </div>
-    </Dialog>
+                {submitError ? (
+                  <div className="b-suberr-div">
+                    {/* biome-ignore lint/a11y/noSvgWithoutTitle: <explanation> */}
+                    <svg className="b-suberr-svg" viewBox="0 0 24 24"
+                      xmlns="http://www.w3.org/2000/svg">
+                      <path d="M12 17q.425 0 .713-.288T13 16t-.288-.712T12 15t-.712.288T11 16t.288.713T12 17m-1-4h2V7h-2zm1 9q-2.075 0-3.9-.788t-3.175-2.137T2.788 15.9T2 12t.788-3.9t2.137-3.175T8.1 2.788T12 2t3.9.788t3.175 2.137T21.213 8.1T22 12t-.788 3.9t-2.137 3.175t-3.175 2.138T12 22m0-2q3.35 0 5.675-2.325T20 12t-2.325-5.675T12 4T6.325 6.325T4 12t2.325 5.675T12 20m0-8" fill="currentColor" />
+                    </svg>
+                    <span>{submitError}</span>
+                  </div>
+                ) : null}
+
+                <div className="b-div3">
+                  <button className='b-divbtn' type="submit">
+                    ログアウト
+                  </button>
+                </div>
+              </form>
+            )}
+          </Form>
+        </div>
+      </Dialog>
+    </>
   );
 };
